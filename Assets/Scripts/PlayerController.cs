9e965d2a7cdf1c687aeeac845e0b280cb359f11e
Sammy Guttman <1449@holbertonschool.com>
2021-12-23 22:20:57 -0500

    task 1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Rigidbody rb;
+	public float speed = 2000;
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Starting");
+    }
+
+	// Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(speed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+           rb.AddForce(-speed * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+           rb.AddForce(0, 0, -speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(0, 0, speed * Time.deltaTime);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
